32f7fdf46f5c47ba1301f52aae3a35a4ae6a5437
Ken Bains <kennbains@gmail.com>
2016-12-22 09:52:06 -0800

    key and title dropdown

--- apps/movie_app/views.py
+++ apps/movie_app/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
+import urllib, json
+
 
 # Create your views here.
 def index(request):
-	return render(request, 'movie_app/index.html')
+	arr =[""]
+	url = "https://data.sfgov.org/api/views/yitu-d5am/rows.json?accessType=DOWNLOAD"
+	response = urllib.urlopen(url)
+	data = json.loads(response.read())
+	for i in data['data']:
+		if arr[len(arr)-1] != i[8]:
+			arr.append(i[8])
+	data = {"titles": arr}
+	return render(request, 'movie_app/index.html', data)
 
 def myfavorites(request):
 	if request.method == "POST":
+		context = {}
 	
-		context = {
-			'location1': request.POST['location1'],
-			'location2': request.POST['location2'],
-			'location3': request.POST['location3'],
-			'location4': request.POST['location4'],
-		}
 		return render(request, 'movie_app/myfavorites.html', context)
